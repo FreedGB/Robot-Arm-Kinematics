a45c6230cc74bf3680a2d75716bc36d7c65feaa5
FreedGB <gfreednel@gmail.com>
2024-03-16 08:20:53 +0100

    Creation of the revolution joint type with up and down movement functions

--- objects.py
+++ objects.py
@@ -2,14 +2,27 @@ from math import pi,cos,sin
 
 class linear_joint:
     # I'll consider that max lenght of the part that goes in and out is equal to the fixed part's lenght
-    def __init__(self,L_fix:float,L_mob:float,initial_angle:float):
+    def __init__(self, L_fix:float, L_mob:float, angle:float):
         self.L_fix = L_fix # the lenght of the fixed part
         self.L_mob = L_mob # the lenght of the mobile part at a particular instant
-        self.initial_angle = initial_angle # the initial angle of the joints' parts. It would be a constant
-        self.end_point_coordinates = [(L_fix + L_mob)*cos(initial_angle) , (L_fix + L_mob)*sin(initial_angle)]
+        self.angle = angle # the initial angle of the joints' parts. It would be a constant
+        self.end_point_coordinates = [(L_fix + self.L_mob)*cos(angle) , (L_fix + self.L_mob)*sin(angle)]
 
-    def move(self,distance:float):
+    def move(self, distance:float):
         self.L_mob += distance
+
+class revo_joint:
+    def __init__(self, L_fix:float, max_angle:float):
+        self.L_fix = L_fix # the lenght of the two parts. It's a constant
+        self.max_angle = max_angle # the max angle the joint can rotate
+        self.angle = pi # the initial joint's angle
+        self.end_point_coordinates = [L_fix*(1+cos(self.angle)) , L_fix*sin(self.angle)]
+
+    def move_up(self, alpha:float):
+        self.angle += alpha
+
+    def move_down(self, alpha:float):
+        self.angle -= alpha
 
 
 
