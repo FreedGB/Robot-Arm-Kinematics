88ccc8511875dee37d4cd547d11ab3debb0b3aad
FreedGB <gfreednel@gmail.com>
2024-03-29 20:34:06 +0100

    Adding an initial position different from [0,0] to the coordinates formulas

--- objects.py
+++ objects.py
@@ -2,21 +2,24 @@ from math import pi,cos,sin
 
 class linear_joint:
     # I'll consider that max lenght of the part that goes in and out is equal to the fixed part's lenght
-    def __init__(self, L_fix:float, L_mob:float, angle:float):
+    def __init__(self, first_point_coordinates:list, L_fix:float, L_mob:float, angle:float):
+        self.first_point_coordinates = first_point_coordinates # the coordinates of the first link
         self.L_fix = L_fix # the lenght of the fixed part
         self.L_mob = L_mob # the lenght of the mobile part at a particular instant
         self.angle = angle # the initial angle of the joints' parts. It would be a constant
-        self.end_point_coordinates = [(L_fix + self.L_mob)*cos(angle) , (L_fix + self.L_mob)*sin(angle)]
+        self.end_point_coordinates = [(first_point_coordinates[0] + L_fix + self.L_mob)*cos(angle) , (first_point_coordinates[1] + L_fix + self.L_mob)*sin(angle)]
 
     def move(self, distance:float):
         self.L_mob += distance
 
 class revo_joint:
-    def __init__(self, L_fix:float, max_angle:float):
+    def __init__(self, first_point_coordinates:list, initial_angle:float, L_fix:float, max_angle:float):
+        self.first_point_coordinates = first_point_coordinates # the coordinates of the first point of the first link
+        self.initial_angle = initial_angle # the initial angle of the first link. It's a constant
         self.L_fix = L_fix # the lenght of the two parts. It's a constant
         self.max_angle = max_angle # the max angle the joint can rotate
         self.angle = pi # the initial joint's angle
-        self.end_point_coordinates = [L_fix*(1+cos(self.angle)) , L_fix*sin(self.angle)]
+        self.end_point_coordinates = [L_fix*(cos(initial_angle) + cos(self.angle + initial_angle)) , L_fix*(sin(initial_angle) + sin(self.angle + initial_angle))]
 
     def move_up(self, alpha:float):
         self.angle += alpha
