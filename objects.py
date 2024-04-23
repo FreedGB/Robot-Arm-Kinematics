eb0731d91be77b869eb6ca1b7eccbebca5e09600
FreedGB <gfreednel@gmail.com>
2024-04-23 17:00:11 +0100

    Adding the move_to(x,y) method for each joint and other optimizations

--- objects.py
+++ objects.py
@@ -1,31 +1,56 @@
-from math import pi,cos,sin
+from math import sqrt,pi,cos,sin
+
 
 class linear_joint:
-    # I'll consider that max lenght of the part that goes in and out is equal to the fixed part's lenght
+    # I'll consider that max lenght of the mobile part as unlimited for now
     def __init__(self, first_point_coordinates:list, L_fix:float, L_mob:float, angle:float):
         self.first_point_coordinates = first_point_coordinates # the coordinates of the first link
         self.L_fix = L_fix # the lenght of the fixed part
         self.L_mob = L_mob # the lenght of the mobile part at a particular instant
-        self.angle = angle # the initial angle of the joints' parts. It would be a constant
-        self.end_point_coordinates = [(first_point_coordinates[0] + L_fix + self.L_mob)*cos(angle) , (first_point_coordinates[1] + L_fix + self.L_mob)*sin(angle)]
+        self.angle = angle # the initial angle of the joint's parts.
+        self.end_point_coordinates = [self.first_point_coordinates[0] + (self.L_fix + self.L_mob)*cos(self.angle) , self.first_point_coordinates[1] + (self.L_fix + self.L_mob)*sin(self.angle)]
+
+    def calculate_coordinates(self):
+        self.end_point_coordinates = [self.first_point_coordinates[0] + (self.L_fix + self.L_mob)*cos(self.angle) , self.first_point_coordinates[1] + (self.L_fix + self.L_mob)*sin(self.angle)]
+
 
     def move(self, distance:float):
+        # Negative distance to go back and positive to go out
         self.L_mob += distance
+        self.calculate_coordinates()
+
+    def move_to(self, x:float, y:float):
+        # This method will allow us to calculate, for every given coordinates, the lenght that the mobile part has to have
+        x0 = self.first_point_coordinates[0]
+        y0 = self.first_point_coordinates[1]
+        new_L_mob = sqrt(((x - x0)**2) + ((y - y0)**2)) - self.L_fix
+        self.move(new_L_mob - self.L_mob)
+
+
+
 
 class revo_joint:
-    def __init__(self, first_point_coordinates:list, initial_angle:float, L_fix:float, max_angle:float):
+    def __init__(self, first_point_coordinates:list, initial_angle:float, L_fix:float):
         self.first_point_coordinates = first_point_coordinates # the coordinates of the first point of the first link
-        self.initial_angle = initial_angle # the initial angle of the first link. It's a constant
+        self.initial_angle = initial_angle # the initial angle of the first link.
         self.L_fix = L_fix # the lenght of the two parts. It's a constant
-        self.max_angle = max_angle # the max angle the joint can rotate
         self.angle = pi # the initial joint's angle
-        self.end_point_coordinates = [L_fix*(cos(initial_angle) + cos(self.angle + initial_angle)) , L_fix*(sin(initial_angle) + sin(self.angle + initial_angle))]
+        self.end_point_coordinates = [self.first_point_coordinates[0] + self.L_fix*cos(self.initial_angle) + self.L_mob*cos(self.angle + self.initial_angle) , self.first_point_coordinates[1] + self.L_fix*sin(self.initial_angle) + self.L_mob*sin(self.angle + self.initial_angle)]
 
-    def move_up(self, alpha:float):
+    def calculate_coordinates(self):
+        self.end_point_coordinates = [self.first_point_coordinates[0] + self.L_fix*cos(self.initial_angle) + self.L_mob*cos(self.angle + self.initial_angle) , self.first_point_coordinates[1] + self.L_fix*sin(self.initial_angle) + self.L_mob*sin(self.angle + self.initial_angle)]
+
+
+    def move(self, alpha:float):
+        # Negative alpha to go down and positive to go up
         self.angle += alpha
+        self.calculate_coordinates()
 
-    def move_down(self, alpha:float):
-        self.angle -= alpha
+    def move_to(self, x:float, y:float):
+        # This method will allow us to calculate, for every given coordinates, the angle that the moveable link have to rotate
+        x0 = self.first_point_coordinates[0]
+        y0 = self.first_point_coordinates[1]
+        
 
 
 
