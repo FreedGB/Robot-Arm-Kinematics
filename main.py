e1b59d169befe75091092d79e0893f0ebda55ed2
FreedGB <gfreednel@gmail.com>
2024-05-05 17:01:35 +0100

    Completing the robot class with the move_to(x,y) method and adding more tests in the main.py file

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from objects import *
 
-revo_joint1 = revo_joint(10,pi/2)
+revo_joint1 = revo_joint(10)
 revo_joint2 = revo_joint(5)
 
 robot = robot_arm_1(revo_joint1,revo_joint2)
-print(robot.grip_coordinates)
+print(f"Grip coordinates = {robot.grip_coordinates}")
+
+robot.move_to(-1,0)
+print(f"Grip coordinates = {robot.grip_coordinates}")
+
+robot.move_to(0,0)
+print(f"Grip coordinates = {robot.grip_coordinates}")
+
+robot.move_to(15,0)
+print(f"Grip coordinates = {robot.grip_coordinates}")
+
+robot.move_to(0,15)
+print(f"Grip coordinates = {robot.grip_coordinates}")
+
+robot.move_to(10,10)
+print(f"Grip coordinates = {robot.grip_coordinates}")
--- objects.py
+++ objects.py
@@ -1,4 +1,4 @@
-from math import sqrt,atan,pi,cos,sin
+from math import sqrt,pi,cos,sin,atan2
 
 
 class linear_joint:
@@ -36,37 +36,91 @@ class revo_joint:
         self.first_point_coordinates = first_point_coordinates # the coordinates of the joint without the link
         self.link_lenght = link_lenght # the lenght of the link at a particular instant
         self.angle = angle # the angle of the joint+link with the x-axis
-        self.end_point_coordinates = [self.first_point_coordinates[0] + self.link_lenght*cos(self.angle) , self.first_point_coordinates[1] + self.link_lenght*sin(self.angle)]
+        self.end_point_coordinates = [round(self.first_point_coordinates[0] + self.link_lenght*cos(self.angle),3) , round(self.first_point_coordinates[1] + self.link_lenght*sin(self.angle),3)]
 
     def calculate_coordinates(self):
         self.end_point_coordinates = [self.first_point_coordinates[0] + self.link_lenght*cos(self.angle) , self.first_point_coordinates[1] + self.link_lenght*sin(self.angle)]
 
 
-    def move(self, alpha:float):
+    def rotate_to(self, alpha:float):
         # Negative alpha to go down and positive to go up
-        self.angle += alpha
+        self.angle = alpha
         self.calculate_coordinates()
 
-    def move_to(self, x:float, y:float):
+    def move_to(self, xn:float, yn:float):
         # This method will allow us to calculate, for every given coordinates, the angle that the moveable link have to rotate
-        x0 = self.first_point_coordinates[0]
-        y0 = self.first_point_coordinates[1]
-        if ((x-x0)**2) + ((y-y0)**2) == (self.link_lenght)**2:
-            new_angle = atan((y - y0) / (x - x0))
-            self.move(new_angle - self.angle)
-        else:
-            print("\nOut of range")
+        x_center = self.first_point_coordinates[0]
+        y_center = self.first_point_coordinates[1]
+        new_angle =  atan2(yn - y_center, xn - x_center)
+        self.rotate_to(new_angle)
+        
+
+            
+
 
 
 # A robot with two revolution joints. The two joints must be defined first
 class robot_arm_1:
     def __init__(self, revo_joint1:revo_joint, revo_joint2:revo_joint):
+        revo_joint2.first_point_coordinates = revo_joint1.end_point_coordinates
+        revo_joint2.calculate_coordinates()
+
         self.revo_joint1 = revo_joint1
         self.revo_joint2 = revo_joint2
 
-        self.base_coordinates = self.revo_joint1.first_point_coordinates
+        self.grip_coordinates = self.revo_joint2.end_point_coordinates
 
+    def calculate_coordinates(self):
+        self.revo_joint1.calculate_coordinates()
         self.revo_joint2.first_point_coordinates = self.revo_joint1.end_point_coordinates
         self.revo_joint2.calculate_coordinates()
+        self.grip_coordinates = self.revo_joint2.end_point_coordinates
 
+    def move_to(self, x:float, y:float):
+
+        if x<0 or y<0:
+            print("\nx and y must be positive or nul.")
+            return
+        
+        L1 = self.revo_joint1.link_lenght
+        L2 = self.revo_joint2.link_lenght
+
+        if round(x**2 + y**2,3) < (L1-L2)**2 or round(x**2 + y**2,3) > (L1+L2)**2:
+            print("\nOut of domain.")
+            return
+        
+        if x == 0 and y == 0:
+            if L1 != L2:
+                print("\nOut of domain.")
+                return
+            
+            x0 = round(L1*cos(pi/4), 3)
+            y0 = round(L1*sin(pi/4), 3)
+
+
+
+        if x == 0 and y != 0:
+            y0 = (L1**2 -L2**2 + y**2)/(2*y)
+            x0 = sqrt(L1**2 - y0**2)
+
+        
+        if x != 0:
+            
+            A = L1**2 - L2**2 + x**2 + y**2
+
+            a = 1 + (y/x)**2
+            b = -(A*y)/(x**2)
+            c = ( (A**2)/(4*x**2) ) - L1**2
+            delta = b**2 - 4*a*c
+
+            if delta >= 0:
+                y0 = round((-b -sqrt(delta))/(2*a), 3)
+                x0 = round(((A/2) - y*y0)/x, 3)
+
+
+        self.revo_joint1.move_to(x0,y0)
+        self.revo_joint1.calculate_coordinates()
+        self.revo_joint2.first_point_coordinates = self.revo_joint1.end_point_coordinates
+        self.revo_joint2.move_to(x,y)
+        self.revo_joint1.calculate_coordinates()
         self.grip_coordinates = self.revo_joint2.end_point_coordinates
